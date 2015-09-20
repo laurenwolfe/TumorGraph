c3af5eb0ee48711bbcfd54d83755b51f7d1baae2
laurenwolfe <lulumialu@gmail.com>
2015-09-19 19:58:50 -0700

    schema.groovy refactor

--- Dockerfile
+++ Dockerfile
@@ -24,9 +24,10 @@ WORKDIR /opt/$TITAN_VER/
 COPY /main/conf/rexster-cassandra-es.xml /opt/$TITAN_VER/conf/
 COPY /main/groovy/PWLoad.groovy /opt/$TITAN_VER/
 COPY /main/groovy/schema.groovy /opt/$TITAN_VER/
+COPY /main/groovy/refactor.groovy /opt/$TITAN_VER/
 COPY /main/groovy/loadToExisting.groovy /opt/$TITAN_VER/
-COPY /data/filenames.tsv /opt/$TITAN_VER/
-COPY /data/stad.all.17jan15.TP.pwpv /opt/$TITAN_VER/
+#COPY /data/filenames.tsv /opt/$TITAN_VER/
+#COPY /data/stad.all.17jan15.TP.pwpv /opt/$TITAN_VER/
 
 RUN mkdir -p /rexhome/ext/titan
 RUN cp -r /lib/*.* /rexhome/ext/titan
--- main/groovy/schema.groovy
+++ main/groovy/schema.groovy
@@ -1,51 +1,74 @@
+class Schema {
 
-g = TitanFactory.build().set("storage.backend", "cassandra").set("storage.hostname", "localhost").set("storage.batch-loading", true).open()
+    //Open gremlin.sh
+    //Load schema: \. schema.groovy
+    //create schema object: schema = new Schema()
+    //call build: g = schema.make()
+    //returns graph object
 
-mgmt = g.getManagementSystem()
+    def make() {
+        //g = TitanFactory.set("storage.backend", "cassandra").set("storage.hostname", "localhost").open()
 
-//*************************//
-// CREATE DATABASE SCHEMA  //
-//                         //
-//*************************//
+        def g = TitanFactory.build().set("storage.backend", "cassandra").set("storage.hostname", "localhost").set("storage.batch-loading", true).open()
+        def mgmt = g.getManagementSystem()
+        def vProp = makeVertexProperties(mgmt)
+        makeEdgeProperties(mgmt)
+        makeLabels(mgmt)
+        buildIndices(mgmt, vProp['objectID'], vProp['type'])
+        commitSchema(mgmt, g)
+        return g
+    }
 
-//This will be generated as "feature_type:geneId"
-objectID = mgmt.makePropertyKey('objectID').dataType(String.class).make()
-//Type of relationship between vertices -- all pairwise for this batch load script
-pairwise = mgmt.makeEdgeLabel('pairwise').multiplicity(Multiplicity.MULTI).make()
-datasetslice = mgmt.makeEdgeLabel('datasetslice').multiplicity(Multiplicity.MULTI).make()
-proximal = mgmt.makeEdgeLabel('proximal').multiplicity(Multiplicity.MULTI).make()
-codesfor = mgmt.makeEdgeLabel('codesfor').multiplicity(Multiplicity.MULTI).make()
-//Identifies these objects as bioentities, as opposed to drugs or other objects we may add later
-bioentity = mgmt.makeVertexLabel('bioentity').make()
+    def makeVertexProperties(mgmt) {
+        //Vertex properties
+        //This will be generated as "tumor_type:feature_type:geneId"
+        def objectID = mgmt.makePropertyKey('objectID').dataType(String.class).make()
+        def type = mgmt.makePropertyKey('type').dataType(String.class).make()
+        def name = mgmt.makePropertyKey('name').dataType(String.class).make()
+        def chr = mgmt.makePropertyKey('chr').dataType(String.class).make()
+        def start = mgmt.makePropertyKey('start').dataType(Integer.class).make()
+        def end = mgmt.makePropertyKey('end').dataType(Integer.class).make()
+        def strand = mgmt.makePropertyKey('strand').dataType(Character.class).make()
+        def tumor_type = mgmt.makePropertyKey('tumor_type').dataType(String.class).make()
+        def version = mgmt.makePropertyKey('version').dataType(String.class).make()
+        def feature_type = mgmt.makePropertyKey('feature_type').dataType(String.class).make()
+        def annotation = mgmt.makePropertyKey('annotation').dataType(String.class).make()
 
-//Vertex properties
-name = mgmt.makePropertyKey('name').dataType(String.class).make()
-chr = mgmt.makePropertyKey('chr').dataType(String.class).make()
-start = mgmt.makePropertyKey('start').dataType(Integer.class).make()
-end = mgmt.makePropertyKey('end').dataType(Integer.class).make()
-strand = mgmt.makePropertyKey('strand').dataType(Character.class).make()
-tumor_type = mgmt.makePropertyKey('tumor_type').dataType(String.class).make()
-version = mgmt.makePropertyKey('version').dataType(String.class).make()
-feature_type = mgmt.makePropertyKey('feature_type').dataType(String.class).make()
-annotation = mgmt.makePropertyKey('annotation').dataType(String.class).make()
-type = mgmt.makePropertyKey('type').dataType(String.class).make()
+        return ['objectID': objectID, 'type': type]
+    }
 
-// See edge-properties.txt for descriptions of these edge properties!
-correlation = mgmt.makePropertyKey('correlation').dataType(Decimal.class).make() //3
-sample_size = mgmt.makePropertyKey('sample_size').dataType(Decimal.class).make() //4
-min_log_p_uncorrected = mgmt.makePropertyKey('min_log_p_uncorrected').dataType(Decimal.class).make() //5
-bonferroni = mgmt.makePropertyKey('bonferroni').dataType(Decimal.class).make() //6
-min_log_p_corrected = mgmt.makePropertyKey('min_log_p_corrected').dataType(Decimal.class).make() //7
-excluded_sample_count_a = mgmt.makePropertyKey('excluded_sample_count_a').dataType(Decimal.class).make() //8
-min_log_p_unused_a = mgmt.makePropertyKey('min_log_p_unused_a').dataType(Decimal.class).make() //9
-excluded_sample_count_b = mgmt.makePropertyKey('excluded_sample_count_b').dataType(Decimal.class).make() //10
-min_log_p_unused_b = mgmt.makePropertyKey('min_log_p_unused_b').dataType(Decimal.class).make() //11
-genomic_distance = mgmt.makePropertyKey('genomic_distance').dataType(Decimal.class).make() //12
-feature_types = mgmt.makePropertyKey('feature_types').dataType(String.class).make()
+    def makeEdgeProperties(mgmt) {
+        // See edge-properties.txt for descriptions of these edge properties!
+        def correlation = mgmt.makePropertyKey('correlation').dataType(Decimal.class).make() //3
+        def sample_size = mgmt.makePropertyKey('sample_size').dataType(Decimal.class).make() //4
+        def min_log_p_uncorrected = mgmt.makePropertyKey('min_log_p_uncorrected').dataType(Decimal.class).make() //5
+        def bonferroni = mgmt.makePropertyKey('bonferroni').dataType(Decimal.class).make() //6
+        def min_log_p_corrected = mgmt.makePropertyKey('min_log_p_corrected').dataType(Decimal.class).make() //7
+        def excluded_sample_count_a = mgmt.makePropertyKey('excluded_sample_count_a').dataType(Decimal.class).make() //8
+        def min_log_p_unused_a = mgmt.makePropertyKey('min_log_p_unused_a').dataType(Decimal.class).make() //9
+        def excluded_sample_count_b = mgmt.makePropertyKey('excluded_sample_count_b').dataType(Decimal.class).make() //10
+        def min_log_p_unused_b = mgmt.makePropertyKey('min_log_p_unused_b').dataType(Decimal.class).make() //11
+        def genomic_distance = mgmt.makePropertyKey('genomic_distance').dataType(Decimal.class).make() //12
+        def feature_types = mgmt.makePropertyKey('feature_types').dataType(String.class).make()
+    }
 
-//Create index of ObjectId to speed map building
-mgmt.buildIndex('byObjectID', Vertex.class).addKey(objectID).unique().buildCompositeIndex()
-mgmt.buildIndex('byType', Vertex.class).addKey(type).buildMixedIndex("search")
-mgmt.commit()
+    def makeLabels(mgmt) {
+        //Type of relationship between vertices -- all pairwise for this batch load script
+        def pairwise = mgmt.makeEdgeLabel('pairwise').multiplicity(Multiplicity.MULTI).make()
+        def datasetslice = mgmt.makeEdgeLabel('datasetslice').multiplicity(Multiplicity.MULTI).make()
+        def proximal = mgmt.makeEdgeLabel('proximal').multiplicity(Multiplicity.MULTI).make()
+        def codesfor = mgmt.makeEdgeLabel('codesfor').multiplicity(Multiplicity.MULTI).make()
+        //Identifies these objects as bioentities, as opposed to drugs or other objects we may add later
+        def bioentity = mgmt.makeVertexLabel('bioentity').make()
+    }
 
-g.commit()
+    def buildIndices(mgmt, objectID, type) {
+        mgmt.buildIndex('byObjectID', Vertex.class).addKey(objectID).unique().buildCompositeIndex()
+        mgmt.buildIndex('byType', Vertex.class).addKey(type).buildMixedIndex("search")
+    }
+
+    def commitSchema(mgmt, g) {
+        mgmt.commit()
+        g.commit()
+    }
+}
